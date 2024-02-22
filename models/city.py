18cdcf6ce845985aaf7f1c3e8201049fd964b658
zaki82000 <yabdelshafy0@gmail.com>
2024-02-22 05:00:25 +0200

    update

--- models/city.py
+++ models/city.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ CIty class """
+    __tablename__ = "cities"
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", cascade="all, delete", backref="cities")
+    else:
+        name = ""
+        state_id = ""
--- models/user.py
+++ models/user.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+""" User module for the HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from os import getenv
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """ Definition of the User class """
+    __tablename__ = "users"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", cascade="all,delete", backref="user")
+        reviews = relationship("Review", cascade="all,delete", backref="user")
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
