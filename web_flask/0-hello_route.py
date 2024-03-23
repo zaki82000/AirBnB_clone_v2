c1a05fbb6c630afecf89651a2c5eaedc3cd9bf17
zaki82000 <yabdelshafy0@gmail.com>
2024-03-23 16:10:09 +0200

    add hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,9 +3,11 @@ from flask import Flask
 
 HBNB_app = Flask(__name__)
 
+
 @HBNB_app.route("/", strict_slashes=False)
 def index():
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
-    HBNB_app.run(debug=True, host='0.0.0.0', port= 5000)
+    HBNB_app.run(debug=True, host='0.0.0.0', port=5000)
