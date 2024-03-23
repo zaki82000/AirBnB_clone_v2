cda37cf05d103d1307224e5222f1db5b2454c8eb
zaki82000 <yabdelshafy0@gmail.com>
2024-03-23 16:15:06 +0200

    add hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module implements a simple Flask application.
+"""
+
 from flask import Flask
 
 hbnb_app = Flask(__name__)
@@ -6,6 +10,10 @@ hbnb_app = Flask(__name__)
 
 @hbnb_app.route("/", strict_slashes=False)
 def index():
+    """
+    This function handles requests to the root URL.
+    It returns a simple greeting message.
+    """
     return "Hello HBNB!"
 
 
