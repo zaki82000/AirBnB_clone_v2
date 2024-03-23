f7c62700ba11bd3e788d6c5a2953b130e82e662e
zaki82000 <yabdelshafy0@gmail.com>
2024-03-23 16:25:25 +0200

    add hbnb_route python file

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module implements a simple Flask application.
+"""
+
+from flask import Flask
+
+hbnb_app = Flask(__name__)
+
+
+@hbnb_app.route("/", strict_slashes=False)
+def index():
+    """
+    This function handles requests to the root URL.
+    It returns a simple greeting message.
+    """
+    return "Hello HBNB!"
+
+@hbnb_app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    This function handles requests to the '/hbnb' URL.
+    It returns a message indicating 'HBNB'.
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    hbnb_app.run(debug=True, host='0.0.0.0', port=5000)
