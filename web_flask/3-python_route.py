d5139d2422b3b0d656477b0b1581eb83a1f8c363
zaki82000 <yabdelshafy0@gmail.com>
2024-03-23 17:11:56 +0200

    add python_route python file

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -36,12 +36,13 @@ def display_c(text):
     return "C " + text.replace("_", " ")
 
 
+@hbnb_app.route("/python/", defaults={'text': "is cool"}, strict_slashes=False)
 @hbnb_app.route("/python/<text>", strict_slashes=False)
 def display_python(text):
     """
     This function handles requests to the '/python/<text>' URL.
-    It returns "python " followed by the value of the text variable
-    with underscores replaced by spaces.
+    It returns "Python " followed by the value of the text variable
+    with underscores replaced by spaces. The default value of text is "is cool"
     """
     return "Python " + text.replace("_", " ")
 
