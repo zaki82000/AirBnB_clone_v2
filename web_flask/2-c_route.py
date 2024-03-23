5b2eb15e8ec4a4f7c27bcf15946f9e8d425dac35
zaki82000 <yabdelshafy0@gmail.com>
2024-03-23 16:59:20 +0200

    add route python file

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+This module implements a simple Flask application.
+"""
+
+from flask import Flask
+
+hbnb_app = Flask(__name__)
+
+
+@hbnb_app.route("/", strict_slashes=False)
+def index():
+    """
+    This function handles requests to the root URL.
+    It returns a simple greeting message.
+    """
+    return "Hello HBNB!"
+
+
+@hbnb_app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    This function handles requests to the '/hbnb' URL.
+    It returns a message indicating 'HBNB'.
+    """
+    return "HBNB"
+
+
+@hbnb_app.route("/c/<text>", strict_slashes=False)
+def display_c(text):
+    """
+    This function handles requests to the '/c/<text>' URL.
+    It returns "C " followed by the value of the text variable
+    with underscores replaced by spaces.
+    """
+    return "C " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    hbnb_app.run(debug=True, host='0.0.0.0', port=5000)
