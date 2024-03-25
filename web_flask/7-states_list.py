36775f2383e826df1ea4e6e014d2882cc3690b1f
zaki82000 <yabdelshafy0@gmail.com>
2024-03-25 16:14:43 +0200

    add states_list python ad html file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,21 +6,23 @@ Starts a Flask web application
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def teardown_session(exception):
-    """Closes the database session after each request"""
+    """
+    Closes the database session after each request
+    """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays a HTML page with a list of all State objects"""
+    """
+    Displays a HTML page with a list of all State objects
+    """
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda x: x.name)
 
