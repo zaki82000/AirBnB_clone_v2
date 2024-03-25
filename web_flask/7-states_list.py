78f532e1e2a73b7346a314b35402fecce12b4a9b
zaki82000 <yabdelshafy0@gmail.com>
2024-03-25 16:19:13 +0200

    add states_list python file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application
-"""
-
+""" starts a Flask web application """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -12,21 +9,16 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """
-    Displays a HTML page with a list of all State objects
-    """
+    """ displays a states_list.html page """
     states = storage.all(State).values()
-
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """
-    Closes the database session after each request
-    """
+def taredown(exeption):
+    """ taredown """
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
